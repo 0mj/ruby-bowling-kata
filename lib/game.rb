2c2cc9da2e0ba50caf0dc7702420bd0ed8d3162e
Matt Jamison <thrdshiftvideo@gmail.com>
2021-06-25 15:00:04 -0400

    green test 3 spare

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,39 @@
 # frozen_string_literal: true
 
 class Game
-  def roll(pins); end
+  FRAMES = 10
+  PINS = 10
+  def initialize
+    @rolls = []
+  end
+
+  def roll(pins)
+    @rolls.push(pins)
+  end
+
+  def score
+    result = 0
+    frame_index = 0
+
+    FRAMES.times do
+      result += if spare?(frame_index)
+                  PINS + spare_bonus(frame_index)  # 10 pins + the spare bonus
+                else
+                  @rolls.fetch(frame_index, 0) + @rolls.fetch(frame_index + 1, 0)
+                end
+      frame_index += 2
+    end
+
+    result
+  end
+
+  private
+
+  def spare?(frame_index)
+    @rolls.fetch(frame_index, 0) + @rolls.fetch(frame_index + 1, 0) == PINS
+  end
+
+  def spare_bonus(frame_index)
+    @rolls.fetch(frame_index + 2, 0)
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,10 +3,36 @@
 require_relative "../lib/game"
 
 RSpec.describe Game do
+  let(:game) { Game.new }
+
+  #   Test 1: Gutter Game
   it "20 rolls of 0 results in a score of 0" do
-    game = Game.new
-    20.times do
-      game.roll(0)
+    roll_many(20, 0)
+    expect(game.score).to eq(0)
+  end
+
+  # Test 2: 10 gutter balls & 10 rolls of 1
+  it "10 rolls of 0 & 19 rolls of 1 results in a score of 10" do
+    roll_many(10, 0)
+    roll_many(10, 1)
+    expect(game.score).to eq(10)
+  end
+
+  # Test 3: The spare
+  it "Rolling 5, 5, 3 followed by 17 rolls of 0 must have a score of 16" do
+    roll_spare
+    game.roll(3)
+    roll_many(17, 0)
+    expect(game.score).to eq(16)
+  end
+
+  def roll_many(rolls, pins)
+    rolls.times do
+      game.roll(pins)
     end
+  end
+
+  def roll_spare
+    roll_many(2, 5)
   end
 end
