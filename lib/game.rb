e3bb303eb409ea3e6d6788ab244fd274dbae6478
Matt Jamison <thrdshiftvideo@gmail.com>
2021-06-25 12:40:53 -0400

    green gutter game

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
 class Game
+  def roll(pins); end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,4 +1,12 @@
+# frozen_string_literal: true
+
 require_relative "../lib/game"
 
 RSpec.describe Game do
+  it "20 rolls of 0 results in a score of 0" do
+    game = Game.new
+    20.times do
+      game.roll(0)
+    end
+  end
 end
