286b6bdf9184b94564aa77221bd70d1036fd0b73
STEVE BERNOODL <thirdshiftvideo@gmail.com>
2022-03-04 07:48:08 -0500

    making commits on wrong branch

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,24 +2,24 @@
 
 require_relative "../lib/game"
 RSpec.describe Game do
+  let(:game) { Game.new }
+
   # 1
   it "20 rolls of 0 has a score of 0" do
-    game = Game.new
-    20.times do
-      game.roll(0)
-    end
+    roll_many(20, 0)
     expect(game.score).to eq(0)
   end
 
   # 2
   it "10 rolls of 0 & 10 rolls of 1 has a score of 10" do
-    game = Game.new
-    10.times do
-      game.roll(0)
-    end
-    10.times do
-      game.roll(1)
-    end
+    roll_many(10, 0)
+    roll_many(10, 1)
     expect(game.score).to eq(10)
+  end
+
+  def roll_many(rolls, pins)
+    rolls.times do
+      game.roll(pins)
+    end
   end
 end
