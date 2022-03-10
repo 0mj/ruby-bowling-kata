d100bf91c19e974f8fabdeb560884cb674faf05d
Matt Jamison <thirdshiftvideo@gmail.com>
2022-03-10 07:51:35 -0500

    test 3 green

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -18,6 +18,15 @@ RSpec.describe Game do
     expect(game.score).to eq(10)
   end
 
+  # 3
+  it "rolling 5, 5, 3, followed by 17 rolls of 0 must have a score of 16" do
+    game.roll(5)
+    game.roll(5)
+    game.roll(3)
+    roll_many(17, 0)
+    expect(game.score).to eq(16)
+  end
+
   def roll_many(rolls, pins)
     rolls.times do
       game.roll(pins)
