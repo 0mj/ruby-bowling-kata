ea560140b35d7ec83b5b4af3723ba34e2f66ba50
Matt Jamison <0mj@users.noreply.github.com>
2022-07-29 09:07:55 -0400

    2022 07 29  034 (#5)
    * 2022-07-29--034
    
    * gutter game test is green
    
    * green through 5 tests

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Game
-  FRAMES = 10
   PINS = 10
+  FRAMES = 10
 
   def initialize
     @rolls = []
@@ -15,11 +15,13 @@ class Game
   def score
     result = 0
     frame_index = 0
+
     FRAMES.times do
       if strike?(frame_index)
         result += PINS + strike_bonus(frame_index)
         frame_index += 1
       elsif spare?(frame_index)
+
         result += PINS + spare_bonus(frame_index)
         frame_index += 2
       else
@@ -41,11 +43,11 @@ class Game
     @rolls.fetch(frame_index + 2, 0)
   end
 
-  def strike_bonus(frame_index)
-    @rolls.fetch(frame_index + 1, 0) + @rolls.fetch(frame_index + 2, 0)
-  end
-
   def strike?(frame_index)
     @rolls.fetch(frame_index, 0) == PINS
+  end
+
+  def strike_bonus(frame_index)
+    @rolls.fetch(frame_index + 1, 0) + @rolls.fetch(frame_index + 2)
   end
 end
