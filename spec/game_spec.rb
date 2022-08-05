d5187a1d0d4041b8fd04c30e6e2af53596678f49
Jamison <thirdshiftvideo@gmail.com>
2022-08-05 10:20:09 -0400

    green through 5

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -15,10 +15,41 @@ RSpec.describe Game do
     roll_many(10, 1)
     expect(game.score).to eq(10)
   end
+
+  # 3 Spare Me! Picking up a spare
+  it "Rolling 5, 5, 3 followed by 17 rolls of 0 must have a score of 16" do
+    roll_spare
+    game.roll(3)
+    roll_many(17, 0)
+    expect(game.score).to eq(16)
+  end
+
+  # 4 YOU DID IT! You gotta strike lil Johnny!
+  it "Rolling 10, 3, 4 followed by 16 rolls of 0 must score 24" do
+    roll_strike
+    game.roll(3)
+    game.roll(4)
+    roll_many(16, 0)
+    expect(game.score).to eq(24)
+  end
+
+  # 5 how many PERFECT GAMEs have there actually been?
+  it "Rolling 12 rolls of 10 must score 300" do
+    roll_many(12, 10)
+    expect(game.score).to eq(300)
+  end
 end
 
 def roll_many(rolls, pins)
   rolls.times do
     game.roll(pins)
   end
+end
+
+def roll_spare
+  roll_many(2, 5)
+end
+
+def roll_strike
+  game.roll(Game::PINS)
 end
