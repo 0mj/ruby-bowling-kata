84fb330a861502e712876f2fa79abe1316e13ade
Jamison <thirdshiftvideo@gmail.com>
2022-09-05 20:27:25 -0400

    38 installing an incorrect gem version causes strange to happen

---
+++ spec/game_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require_relative "../lib/game"
+RSpec.describe Game do
+  it "Rolling 20 rolls of 0 must score 0" do
+    game = Game.new
+    20.times do
+      game.roll(0)
+    end
+
+    expect(game.score).to eq(0)
+  end
+
+  it "10 rolls of 0 and 10 rolls of 1 must score 10" do
+    game = Game.new
+    10.times do
+      game.roll(0)
+    end
+    10.times do
+      game.roll(1)
+    end
+    expect(game.score).to eq(10)
+  end
+end
