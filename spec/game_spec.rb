76c0487c7e4d4edfb8b7917b3fd21f01d7d3e9b4
Jamison <thirdshiftvideo@gmail.com>
2022-09-12 08:00:09 -0400

    039

---
+++ spec/game_spec.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require_relative "../lib/game"
+RSpec.describe Game do
+  let(:game) { Game.new }
+
+  it "Rolling 20 rolls of 0 must score 0" do
+    20.times do
+      game.roll(0)
+    end
+    expect(game.score).to eq(0)
+  end
+
+  it "Rolling 10 rolls of 0 and 10 rolls of 1 must score 10" do
+    roll_many(10, 0)
+    roll_many(10, 1)
+    expect(game.score).to eq(10)
+  end
+
+  it "Rolling 5, 5, 3 followed by 17 rolls of 0 must score 16" do
+    roll_spare
+    game.roll(3)
+    roll_many(17, 0)
+    expect(game.score).to eq(16)
+  end
+
+  it "Rolling 10" do
+    game.roll(10)
+    game.roll(3)
+    game.roll(4)
+    expect(game.score).to eq(24)
+  end
+
+  it "Rolling 12 rolls of 10 must score 300" do
+    roll_many(12, 10)
+    expect(game.score).to eq(300)
+  end
+end
+
+def roll_many(rolls, pins)
+  rolls.times do
+    game.roll(pins)
+  end
+end
+
+def roll_spare
+  roll_many(2, 5)
+end
