f9fc66f415bbcfc01f5709733c2b0a0eb5c3f046
Jamison <thirdshiftvideo@gmail.com>
2022-09-28 21:21:32 -0400

    40 test 2 green

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
+
 require_relative "../lib/game"
 
 RSpec.describe Game do
-
   it "Rolling 20 rolls of 0 must score 0" do
     game = Game.new
     20.times do
@@ -9,5 +10,16 @@ RSpec.describe Game do
     end
     expect(game.score).to eq(0)
   end
-  
+
+  it "Rolling 10 rolls of 1 and 10 rolls of 0 must score 10" do
+    game = Game.new
+    10.times do
+      game.roll(1)
+    end
+    10.times do
+      game.roll(0)
+    end
+
+    expect(game.score).to eq(10)
+  end
 end
