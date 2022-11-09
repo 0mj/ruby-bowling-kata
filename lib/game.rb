8ee637a5df5ee2635d605785d01a8849959ee4b2
Jamison <thirdshiftvideo@gmail.com>
2022-11-09 08:52:54 -0500

    43 green all day

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Game
+  FRAMES = 10
+  PINS = 10
+
   def initialize
     @rolls = []
   end
@@ -13,15 +16,37 @@ class Game
     result = 0
     frame_index = 0
 
-    10.times do
-      result += if @rolls.fetch(frame_index, 0) + @rolls.fetch(frame_index + 1, 0) == 10
-        10 + @rolls.fetch(frame_index + 2, 0)
+    FRAMES.times do
+      if strike?(frame_index)
+        result += PINS + strike_bonus(frame_index)
+        frame_index += 1
+      elsif spare?(frame_index)
+        result += PINS + spare_bonus(frame_index)
+        frame_index += 2
       else
-        @rolls.fetch(frame_index, 0) + @rolls.fetch(frame_index + 1, 0)
+        result += @rolls.fetch(frame_index, 0) + @rolls.fetch(frame_index + 1, 0)
+        frame_index += 2
       end
-      frame_index += 2
     end
 
     result
+  end
+
+  private
+
+  def spare?(frame_index)
+    @rolls.fetch(frame_index, 0) + @rolls.fetch(frame_index + 1, 0) == PINS
+  end
+
+  def spare_bonus(frame_index)
+    @rolls.fetch(frame_index + 2, 0)
+  end
+
+  def strike?(frame_index)
+    @rolls.fetch(frame_index, 0) == PINS
+  end
+
+  def strike_bonus(frame_index)
+    @rolls.fetch(frame_index + 1, 0) + @rolls.fetch(frame_index + 2, 0)
   end
 end
