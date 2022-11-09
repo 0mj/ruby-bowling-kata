1f3438b7b8de47a30e516437849461f5e1a671d1
Jamison <thirdshiftvideo@gmail.com>
2022-11-09 08:25:04 -0500

    43 needs a commit message

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,20 +2,20 @@
 
 require_relative "../lib/game"
 RSpec.describe Game do
-  let(:game) {Game.new}
-  
+  let(:game) { Game.new }
+
   it "Rolling 20 rolls of 0 must score 0" do
-    roll_many(20,0)
+    roll_many(20, 0)
     expect(game.score).to eq(0)
   end
 
   it "10 gutter balls & 10 rolls of 1 must score 10" do
-    roll_many(10,1)
-    roll_many(10,0)
+    roll_many(10, 1)
+    roll_many(10, 0)
     expect(game.score).to eq(10)
   end
 
-  def roll_many(rolls,pins)
+  def roll_many(rolls, pins)
     rolls.times do
       game.roll(pins)
     end
