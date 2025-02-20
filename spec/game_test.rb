239aff45f5d0fcbfdf8fd3afa5ddb15bbdc7664e
Matt Jamison <thirdshiftvideo@gmail.com>
2025-02-20 09:24:35 -0500

    test zero

--- spec/game_test.rb
+++ spec/game_test.rb
@@ -1,12 +1,3 @@
-require_relative "../lib/game"
-require "MiniTest/autorun"
-
 class TestGame < MiniTest::Test
-  
-  def test_game_class_exists
-    game = Game.new
-    assert_instance_of Game, game
-  end
-  
 
 end
