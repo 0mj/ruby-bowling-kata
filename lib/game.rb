7907f4e3da46a51309bc53572e32f11958154df4
Matt Jamison <thirdshiftvideo@gmail.com>
2025-02-21 10:39:36 -0500

    tests passing through test 2

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,7 @@
 class Game
 
-  FRAMES = 10
-  PINS   = 10 
-
   def initialize
-    @rolls = []
+    @rolls=[]  
   end
 
   def roll(pins)
@@ -12,43 +9,7 @@ class Game
   end
 
   def score
-    result = 0
-    frame_index = 0
-
-    FRAMES.times do #Frames in a game
-      if @rolls.fetch(frame_index, 0) == PINS #strike!
-        result += PINS + strike_bonus(frame_index)
-        frame_index += 1
-      elsif spare?(frame_index)
-        result += PINS + spare_bonus(frame_index) 
-        frame_index += 2
-      else
-        result += @rolls.fetch(frame_index, 0) + @rolls.fetch(frame_index + 1, 0) 
-        frame_index += 2
-      end
-      
-    end
-
-    result
-  end
-
-
-  private 
-
-  def spare?(frame_index)
-    @rolls.fetch(frame_index, 0) + @rolls.fetch(frame_index + 1, 0) == 10 #spare
-  end
-
-  def spare_bonus(frame_index)
-    @rolls.fetch(frame_index + 2, 0)
-  end
-
-  def strike?(frame_index)
-    @rolls.fetch(frame_index, 0) == PINS #strike!
-  end
-
-  def strike_bonus(frame_index)
-    @rolls.fetch(frame_index + 1, 0) + @rolls.fetch(frame_index + 2, 0) # strike bonus adds next to balls
+    @rolls.sum
   end
 
 end
--- spec/game_test.rb
+++ spec/game_test.rb
@@ -11,7 +11,7 @@ class TestGame < MiniTest::Test
     assert_instance_of Game, @game
   end
 
-  # 20 rolls of zero must score zero
+  # Gutter game. 20 rolls of zero must score zero
   def test_gutter_game
     20.times do
       @game.roll(0)
@@ -19,46 +19,20 @@ class TestGame < MiniTest::Test
     assert_equal 0, @game.score
   end
 
-  # 10 rolls of 1 & 10 rolls of zero
+  # 10 gutter balls & 10 rolls of 1 must score 10
   def test_ten_gutter_ten_one
-    roll_many(10, 0)
-    roll_many(10, 1)
+    roll_many(10,0)
+    roll_many(10,1)
     assert_equal 10, @game.score
   end
 
-  def test_spare
-    @game.roll(5)
-    @game.roll(5)
-    @game.roll(3)
-    assert_equal 16, @game.score
-  end
-
   def roll_many(rolls, pins)
-    rolls.times do
+    rolls.times do 
       @game.roll(pins)
     end
   end
 
-  def roll_spare
-    roll_many(2,5)
-  end
 
-  def test_strike
-    roll_strike
-    @game.roll(3)
-    @game.roll(4)
-    roll_many(16,0)
-    assert_equal 24, @game.score
-  end
-
-  def test_perfect_game
-    roll_many(12, Game::PINS)
-    assert_equal 300, @game.score
-  end
-
-  def roll_strike
-    @game.roll(10)
-  end
 
 
 end
