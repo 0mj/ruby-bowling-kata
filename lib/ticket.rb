2b7c66152eb939fc3bb7ad858eb9614eb038c84f
Matt Jamison <thirdshiftvideo@gmail.com>
2025-04-20 21:23:35 -0400

    price, discount, date

--- lib/ticket.rb
+++ lib/ticket.rb
@@ -1,3 +1,39 @@
 class Ticket
+  def initialize(venue = 'venue')
+    @venue = venue
+    @date = date
+  end
 
+  def venue
+    @venue
+  end
+
+  def date
+    @date
+  end
+
+  def price=(amount)
+    if(amount * 100).to_i == amount * 100
+      @price = amount
+    else
+      puts 'price is malformed'
+    end
+  end
+
+  def price
+    @price
+  end
+
+  def discount=(percent)
+    
+    discount = @price * percent
+    @price = @price - discount
+  end
+
+  def date=(string)
+    string =~ /^\d{4}-\d{2}-\d{2}$/
+    if string != 'yyyy-mm-dd'
+      puts 'try again fawker'
+    end
+  end
 end
--- spec/ticket_test.rb
+++ spec/ticket_test.rb
@@ -3,6 +3,31 @@ require_relative '../lib/ticket'
 
 class TicketTest < MiniTest::Test
 
+  def setup
+    @ticket = Ticket.new
+  end
+
+  def test_ticket_exists
+    assert_instance_of Ticket, @ticket
+  end
+
+  def test_default_ticket_values
+    assert_equal 'venue', @ticket.venue
+  end
+
+  def test_price
+    assert_equal 10.50, @ticket.price= 10.50
+  end
+
+  def test_discount
+    @ticket.price= 100
+    @ticket.discount= 0.15
+    assert_equal 85.00, @ticket.price
+  end
+
+  def test_ticket_date_set
+    @ticket.date= '2025-04-20'
+  end
 
 
 end
