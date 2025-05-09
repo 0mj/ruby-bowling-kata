cc91ab3fd55bfe0f88669dcd9a2d1c7c8cb5693f
Matt Jamison <thirdshiftvideo@gmail.com>
2025-05-09 11:06:32 -0400

    practice

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,13 @@
 class Game
-
-  PINS = 10
   FRAMES = 10
+  PINS = 10
+
+  
+  attr_accessor :rolls, :frames
 
   def initialize
     @rolls = []
+    @frames = []
   end
 
   def roll(pins)
@@ -12,29 +15,74 @@ class Game
   end
 
   def score
-    result = 0 
-    frame_index = 0 
-
-    FRAMES.times do
-      if @rolls.fetch(frame_index,0) == PINS #strike
-        result += PINS + @rolls.fetch(frame_index + 1, 0) + @rolls.fetch(frame_index + 2, 0)
-        frame_index += 1
-      elsif spare?(frame_index)
-        result += spare_bonus(frame_index)
-        frame_index += 2
-      else
-        result += @rolls.fetch(frame_index, 0) + @rolls.fetch(frame_index + 1, 0)
-        frame_index += 2
-      end
-    end
-    result
+    calculate_score
+    @frames.sum { |frame| frame[:score] } 
   end
 
   private
+
+  def calculate_score
+    @frames.clear  
+    roll_index = 0
+
+    FRAMES.times do |frame_number|
+      roll1 = @rolls.fetch(roll_index, 0)
+      roll2 = @rolls.fetch(roll_index + 1, 0)
+      frame_score = 0
+      frame_rolls = [roll1]
+
+      if strike?(roll_index)
+        frame_score = PINS + @rolls.fetch(roll_index + 1, 0) + @rolls.fetch(roll_index + 2, 0)
+        frame_rolls << @rolls.fetch(roll_index + 1, 0)
+        frame_rolls << @rolls.fetch(roll_index + 2, 0)
+        roll_index += 1
+        @frames << { frame: frame_number + 1, type: :strike, rolls: frame_rolls, score: frame_score }
+      elsif spare?(roll_index)
+        frame_score = PINS + @rolls.fetch(roll_index + 2, 0)
+        frame_rolls << roll2
+        frame_rolls << @rolls.fetch(roll_index + 2, 0)
+        roll_index += 2
+        @frames << { frame: frame_number + 1, type: :spare, rolls: frame_rolls, score: frame_score }
+      else
+        frame_score = roll1 + roll2
+        frame_rolls << roll2
+        roll_index += 2
+        @frames << { frame: frame_number + 1, type: :open, rolls: frame_rolls, score: frame_score }
+      end
+    end
+     
+    calculate_tenth_frame_score
+  end
+
+  def calculate_tenth_frame_score
+     last_frame = @frames.last
+     roll_index = @rolls.size - last_frame[:rolls].size
+     roll1 = @rolls.fetch(roll_index, 0)
+     roll2 = @rolls.fetch(roll_index + 1, 0)
+     roll3 = @rolls.fetch(roll_index + 2, 0)
+    if @frames.size == 10
+        if strike?(roll_index)
+            last_frame[:score] = 10 + (@rolls.fetch(roll_index + 1, 0) || 0) + (@rolls.fetch(roll_index + 2, 0) || 0)
+        elsif spare?(roll_index)
+             last_frame[:score] = 10 + (@rolls.fetch(roll_index + 2, 0) || 0)
+        else
+            last_frame[:score] = roll1 + roll2
+        end
+    end
+  end
   def spare?(frame_index)
     @rolls.fetch(frame_index, 0) + @rolls.fetch(frame_index + 1, 0) == PINS
   end
+
   def spare_bonus(frame_index)
     PINS + @rolls.fetch(frame_index + 2, 0)
+  end
+
+  def strike?(frame_index)
+    @rolls.fetch(frame_index, 0) == PINS
+  end
+
+  def strike_bonus(frame_index)
+     PINS + @rolls.fetch(frame_index + 1, 0) + @rolls.fetch(frame_index + 2, 0)
   end
 end
--- spec/game_test.rb
+++ spec/game_test.rb
@@ -2,7 +2,6 @@ require 'minitest/autorun'
 require_relative '../lib/game'
 
 class GameTest < Minitest::Test
-
   def setup
     @game = Game.new
   end
@@ -12,39 +11,88 @@ class GameTest < Minitest::Test
   end
 
   def test_gutter_game
-    roll_many(20,0)
+    roll_many(20, 0)
     assert_equal 0, @game.score
+    assert_equal 10, @game.frames.size
+    @game.frames.each do |frame|
+      assert_equal :open, frame[:type]
+    end
   end
 
-  def test_ten_ones_ten_gutter
-    roll_many(10,0)
-    roll_many(10,1)
+  def test_ten_ones_ten_gutters
+    roll_many(10, 1)
+    roll_many(10, 0)
     assert_equal 10, @game.score
+    assert_equal 10, @game.frames.size
+    @game.frames.each do |frame|
+      assert_equal :open, frame[:type]
+    end
   end
 
-  # roll [9,1][6,3][7,1] & 14 gutters
   def test_spare
-    roll_frame(9,1) #spare
-    roll_frame(6,3)
-    roll_frame(7,1)
-    roll_many(14,0)
-    assert_equal 33, @game.score  
+    roll_frame(5, 5) # spare
+    roll_frame(9, 0)
+    roll_frame(4, 0)
+    roll_frame(7, 2)
+    roll_many(12, 0)
+    assert_equal 41, @game.score
+    assert_equal 10, @game.frames.size
+    assert_equal :spare, @game.frames[0][:type]
+    assert_equal :open, @game.frames[1][:type]
   end
 
   def test_strike
     @game.roll(Game::PINS)
-    roll_frame(9,0)
-    roll_frame(9,0)
-    roll_many(14,0)
-    assert_equal 37, @game.score
+    roll_frame(8, 1)
+    roll_many(16, 0)
+    assert_equal 28, @game.score
+    assert_equal 10, @game.frames.size
+    assert_equal :strike, @game.frames[0][:type]
+    assert_equal :open, @game.frames[1][:type]
   end
 
   def test_perfect_game
-    roll_many(12,Game::PINS)
+    roll_many(12, Game::PINS)
     assert_equal 300, @game.score
+    assert_equal 10, @game.frames.size
+    @game.frames.each do |frame|
+      assert_equal :strike, frame[:type]
+    end
+  end
+
+  def test_all_spares
+    10.times do
+      roll_frame(5, 5)
+    end
+    @game.roll(5)  # Extra ball for the spare in the 10th frame
+    assert_equal 150, @game.score
+    assert_equal 10, @game.frames.size
+    @game.frames.each do |frame|
+      assert_equal :spare, frame[:type]
+    end
+  end
+
+  def test_random_game
+     10.times do
+       roll1 = rand(0..10)
+       if roll1 == 10
+         @game.roll(roll1)
+       else
+        roll2 = rand(0..(10 - roll1))
+        @game.roll(roll1)
+        @game.roll(roll2)
+       end
+     end
+    puts "Rolls for this game: #{@game.rolls}"
+    puts "Frames for this game: #{@game.frames.inspect}"
+    puts "Complete game score: #{@game.score}"
+    assert_equal 10, @game.frames.size
+    assert @game.score >= 0
+    assert @game.score <= 300
   end
 
   private
+
   def roll_many(rolls, pins)
     rolls.times do
       @game.roll(pins)
@@ -55,5 +103,4 @@ class GameTest < Minitest::Test
     @game.roll(r1)
     @game.roll(r2)
   end
-
 end
