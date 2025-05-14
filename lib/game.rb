d0ed4061686ff40521caa8acb5fa9e3dc52a2976
Matt Jamison <thirdshiftvideo@gmail.com>
2025-05-14 11:02:37 -0400

    practice

--- lib/game.rb
+++ lib/game.rb
@@ -1,32 +1,56 @@
 class Game
   FRAMES = 10
   PINS = 10
+  attr_accessor :frames, :rolls
+
   def initialize
     @rolls = []
+    @frames = []
   end
   def roll(pins)
     @rolls << pins
   end
   def score
     result = 0
-    roll_index = 0 
+    roll_index = 0
+    frame = 0 
 
     FRAMES.times do
+      frame += 1
+      
       if strike?(roll_index)
         result += strike_bonus(roll_index)
+        @frames << { frame: frame,  box_score: [box_strike],  score: result } 
         roll_index += 1
+      
       elsif spare?(roll_index)
         result += spare_bonus(roll_index)
+        @frames << { frame: frame, box_score: box_spare(roll_index), score: result }
         roll_index += 2
+        
       else
         result += @rolls.fetch(roll_index, 0) + @rolls.fetch(roll_index + 1,0) 
+        @frames << { frame: frame, box_score: box_open(roll_index), score: result }
         roll_index += 2
+        
       end
+
     end
     result
   end
+  def box_strike
+    "X"
+  end
+  def box_spare(roll_index)
+    [@rolls.fetch(roll_index, 0),"/"]    
+  end
 
+  def box_open(roll_index)
+    [@rolls.fetch(roll_index, 0), @rolls.fetch(roll_index + 1,0)]
+  end
+  
   private
+ 
   def strike?(roll_index)
     @rolls.fetch(roll_index, 0) == PINS #STRIKE!
   end
--- spec/game_test.rb
+++ spec/game_test.rb
@@ -32,7 +32,7 @@ class GameTest < Minitest::Test
   end
 
   def test_strike
-    @game.roll(Game::PINS)
+    strike
     roll_frame(7,2)
     roll_many(16,0)
     assert_equal 28, @game.score
@@ -43,7 +43,34 @@ class GameTest < Minitest::Test
     assert_equal 300, @game.score
   end
 
+  def test_frames_array
+    strike
+    roll_frame(5,4)
+    roll_many(16,0)
+    @game.score
+    @game.frames.each do |frame|
+      assert_equal [:frame, :box_score, :score].sort, frame.keys.sort, "Frame should have keys: :frame, :box_score, :score"
+      assert_instance_of Integer, frame[:frame], "Frame number should be an Integer"
+      assert_instance_of Array, frame[:box_score], "Rolls should be an Array"
+      assert_instance_of Integer, frame[:score], "Score should be an Integer"
+    end
+  end
+
+  # Box score represents the 2 boxes at top of frame box.  Frame box holds running score and box score holds indiviidual roll.
+  def test_box_score
+    strike
+    roll_frame(9,0)
+    roll_frame(7,3)
+    roll_frame(5,3)
+    roll_many(12,0)
+    @game.score
+    puts @game.frames
+  end
+
   private
+  def strike
+    @game.roll(Game::PINS)
+  end
   def roll_many(rolls, pins)
     rolls.times do
       @game.roll(pins)
