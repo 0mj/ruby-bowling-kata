df6fa86572e1a52429aaecd47802f2b67ee13c84
Matt Jamison <thirdshiftvideo@gmail.com>
2025-05-20 17:04:50 -0400

    practice + scorecard has created during score method

--- lib/game.rb
+++ lib/game.rb
@@ -1,106 +1,63 @@
 class Game
-
-  FRAMES = 10
   PINS = 10
-<<<<<<< Updated upstream
-  attr_accessor :frames, :rolls
-=======
-
-  attr_reader :frames
->>>>>>> Stashed changes
-
+  FRAMES = 10
+  attr_reader :scorecard
   def initialize
     @rolls = []
-    @frames = []
+    @scorecard = []
   end
 
   def roll(pins)
-    @rolls.push(pins)
+    @rolls << pins
   end
 
   def score
-    result = 0
-<<<<<<< Updated upstream
-    roll_index = 0
-    frame = 0 
-
+    result = 0 
+    roll_ndx = 0
+    frame = 0
     FRAMES.times do
-      frame += 1
-      
-      if strike?(roll_index)
-        result += strike_bonus(roll_index)
-        @frames << { frame: frame,  box_score: [box_strike],  score: result } 
-        roll_index += 1
-      
-      elsif spare?(roll_index)
-        result += spare_bonus(roll_index)
-        @frames << { frame: frame, box_score: box_spare(roll_index), score: result }
-        roll_index += 2
-        
+      if strike?(roll_ndx)
+        result += strike_bonus(roll_ndx)
+        write_scorecard(frame, result, "strike", [PINS, "X"])
+        roll_ndx += 1
+        frame += 1
+      elsif spare?(roll_ndx)
+        result += spare_bonus(roll_ndx)
+        write_scorecard(frame, result, "spare", [@rolls.fetch(roll_ndx,0), @rolls.fetch(roll_ndx + 1,0), "/"])
+        roll_ndx += 2
+        frame += 1
       else
-        result += @rolls.fetch(roll_index, 0) + @rolls.fetch(roll_index + 1,0) 
-        @frames << { frame: frame, box_score: box_open(roll_index), score: result }
-        roll_index += 2
-        
+        result += open_frame(roll_ndx)
+        write_scorecard(frame, result, "open", [@rolls.fetch(roll_ndx,0), @rolls.fetch(roll_ndx + 1,0)])
+        roll_ndx += 2
+        frame += 1
       end
-
-=======
-    roll_i = 0
-    frame  = 0
-
-    FRAMES.times do
-      
-      if strike?(roll_i)
-        result += strike_bonus(roll_i)
-        roll_i += 1
-      elsif spare?(roll_i)
-        result += spare_bonus(roll_i)
-        roll_i += 2 
-      else
-        result += open_frame(roll_i)
-        @frames <<  box_score(roll_i) # @frames << {frame: frame + 1, box_score: box_score(roll_i) }
-        roll_i += 2  
-      end  
-      
->>>>>>> Stashed changes
+        
     end
     result
   end
-  def box_strike
-    "X"
-  end
-  def box_spare(roll_index)
-    [@rolls.fetch(roll_index, 0),"/"]    
-  end
 
-  def box_open(roll_index)
-    [@rolls.fetch(roll_index, 0), @rolls.fetch(roll_index + 1,0)]
+
+  private
+  def spare_bonus(roll_ndx)
+    PINS + @rolls.fetch(roll_ndx + 2, 0)
+  end
+  def spare?(roll_ndx)
+    @rolls.fetch(roll_ndx, 0) + @rolls.fetch(roll_ndx + 1, 0) == PINS
+  end
+  def strike?(roll_ndx)
+    @rolls.fetch(roll_ndx, 0) == PINS
+  end
+  def strike_bonus(roll_ndx)
+    PINS + @rolls.fetch(roll_ndx + 2, 0) + @rolls.fetch(roll_ndx + 1, 0)
+  end
+  def open_frame(roll_ndx)
+    @rolls.fetch(roll_ndx, 0) + @rolls.fetch(roll_ndx + 1, 0)
+  end
+  def write_scorecard(frame, result, type, rolls)
+   
+      @scorecard << {frame: frame + 1, framescore: result, type: type, rolls: rolls }
+   
   end
   
-  private
-<<<<<<< Updated upstream
- 
-  def strike?(roll_index)
-    @rolls.fetch(roll_index, 0) == PINS #STRIKE!
-=======
-  def box_score(roll_i)
-    [@rolls.fetch(roll_i, 0), @rolls.fetch(roll_i + 1, 0)]  
->>>>>>> Stashed changes
-  end
-  def strike?(roll_i)
-    @rolls.fetch(roll_i,0) == PINS 
-  end
-  def strike_bonus(roll_i)
-    PINS + @rolls.fetch(roll_i + 1, 0) + @rolls.fetch(roll_i + 2, 0)
-  end
-  def spare?(roll_i)
-    @rolls.fetch(roll_i, 0) + @rolls.fetch(roll_i + 1, 0) == PINS
-  end
-  def spare_bonus(roll_i)
-    PINS + @rolls.fetch(roll_i + 2,0)
-  end
-  def open_frame(roll_i)
-    @rolls.fetch(roll_i, 0) + @rolls.fetch(roll_i + 1, 0)
-  end
-
 end
--- spec/game_test.rb
+++ spec/game_test.rb
@@ -1,96 +1,95 @@
-require 'minitest/autorun'
 require_relative '../lib/game'
+require 'minitest/autorun'
 
 class GameTest < Minitest::Test
 
   def setup
     @game = Game.new
   end
-  def test_game_class
-    
+
+  def test_game_exists
     assert_instance_of Game, @game
   end
 
-  def test_gutters
+  def test_gutter_game
     roll_many(20,0)
     assert_equal 0, @game.score
   end
 
-  def test_ones
-    roll_many(20,1)
-    assert_equal 20, @game.score
+  def test_10_gutter_10_one
+    roll_many(10,0)
+    roll_many(10,1)
+    assert_equal 10, @game.score
   end
 
   def test_spare
-    roll_frame(8,2)
-    roll_frame(8,0)
-    roll_many(16,0)
-    assert_equal 26, @game.score
+    complete_frame(9,1)
+    complete_frame(9,0)
+    complete_frame(5,3)
+    roll_many(14,0)
+    assert_equal 36, @game.score
   end
 
-  def test_strike
-    strike
-<<<<<<< Updated upstream
-    roll_frame(7,2)
-=======
-    roll_frame(9,0)
->>>>>>> Stashed changes
-    roll_many(16,0)
-    assert_equal 28, @game.score
+  def test_strike_spare
+    @game.roll(Game::PINS) # 10 + 4 + 6 = 20
+    complete_frame(4,6) # 4 + 6 + 9 + 20 = 39
+    complete_frame(9,0) # 9 + 0 + 39 = 48
+    roll_many(14,0)
+    assert_equal 48, @game.score
+    assert_includes @game.scorecard,  { frame: 1, framescore: 20, type: "strike", rolls: [10,"X"]}
+    assert_includes @game.scorecard,  { frame: 2, framescore: 39, type: "spare", rolls: [4,6,"/"]}
+    assert_includes @game.scorecard,  { frame: 3, framescore: 48, type: "open", rolls: [9,0]}
+    # puts @game.scorecard
   end
 
   def test_perfect
     roll_many(12, Game::PINS)
     assert_equal 300, @game.score
+    expected_hash = { frame: 1, framescore: 30, type: "strike", rolls: [10,"X"]}
+    assert_includes @game.scorecard, expected_hash
+
+    expected_hash = { frame: 10, framescore: 300, type: "strike", rolls: [10,"X"]}
+    assert_includes @game.scorecard, expected_hash
+    # puts @game.scorecard
   end
 
-<<<<<<< Updated upstream
-  def test_frames_array
-    strike
-    roll_frame(5,4)
-    roll_many(16,0)
-    @game.score
-    @game.frames.each do |frame|
-      assert_equal [:frame, :box_score, :score].sort, frame.keys.sort, "Frame should have keys: :frame, :box_score, :score"
-      assert_instance_of Integer, frame[:frame], "Frame number should be an Integer"
-      assert_instance_of Array, frame[:box_score], "Rolls should be an Array"
-      assert_instance_of Integer, frame[:score], "Score should be an Integer"
-    end
+  def test_scorecard_open
+    assert_equal [], @game.scorecard
+    @game.roll(5)
+    assert_equal 5, @game.score
+    expected_hash = { frame: 1, framescore: 5, type: "open", rolls: [5,0]}
+    assert_includes @game.scorecard, expected_hash
   end
 
-  # Box score represents the 2 boxes at top of frame box.  Frame box holds running score and box score holds indiviidual roll.
-  def test_box_score
-    strike
-    roll_frame(9,0)
-    roll_frame(7,3)
-    roll_frame(5,3)
-    roll_many(12,0)
+  def test_scorecard_rolls
+    complete_frame(4,5)
     @game.score
-    puts @game.frames
-=======
-  def test_box_score
-    roll_frame(4,3)
-    roll_many(18,0)
-    @game.score
-    assert_equal  [[4, 3], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]], @game.frames
->>>>>>> Stashed changes
+    assert_includes @game.scorecard, { frame: 1, framescore: 9, type: "open", rolls: [4,5]}
   end
+
+  def test_strike_in_tenth
+    roll_many(18, 0)
+    strike # strike in 10th get 2 bonus balls | spare in 10th gets 1 bonus ball
+    strike
+    strike
+    assert_equal 30, @game.score
+    # assert_includes @game.scorecard, { frame: "10_bonus_1", framescore: "", type: "strike", rolls: []}
+    # puts @game.scorecard  # DISPLAY bonus rolls in 10th...
+  end
+
+
 
   private
-  def strike
-    @game.roll(Game::PINS)
-  end
   def roll_many(rolls, pins)
     rolls.times do
       @game.roll(pins)
     end
   end
-  def roll_frame(roll1, roll2)
+  def complete_frame(roll1,roll2)
     @game.roll(roll1)
     @game.roll(roll2)
   end
   def strike
     @game.roll(Game::PINS)
   end
-
 end
