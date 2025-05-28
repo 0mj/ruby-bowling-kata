1608142dd2b3c4c5cf0e13628f1a5264d6a922e5
Matt Jamison <thirdshiftvideo@gmail.com>
2025-05-28 15:47:19 -0400

    tenth frame tests

--- lib/game.rb
+++ lib/game.rb
@@ -1,26 +1,41 @@
 class Game
   FRAMES = 10
   PINS = 10
+  attr_accessor :card
   def initialize
     @rolls = []
+    @card = []
   end
   def roll(pins)
     @rolls << pins
   end
 
+  def tenth_frame_strike?
+    # @card.include?({frame: 10, type: "STRIKE", frame_score: Game::PINS })
+    @card.any? { |f| f[:frame] == 10 && f[:type] == "STRIKE" }
+  end
+
   def score
     result = 0
     rollndx = 0
+    crd_frm = 0
 
     FRAMES.times do
       if strike?(rollndx)
-        result += strike_bonus(rollndx)
+        frame_score = strike_bonus(rollndx)
+        result += frame_score
+        @card << { frame: crd_frm += 1, type: "STRIKE", frame_score: frame_score, total: result }
         rollndx += 1
+
       elsif spare?(rollndx)
-        result += spare_bonus(rollndx)
+        frame_score = spare_bonus(rollndx)
+        result += frame_score
+        @card << { frame: crd_frm += 1, type: "SPARE", frame_score: frame_score, total: result }
         rollndx += 2
       else 
-        result += @rolls.fetch(rollndx, 0) + @rolls.fetch(rollndx + 1, 0)
+        frame_score = @rolls.fetch(rollndx, 0) + @rolls.fetch(rollndx + 1, 0)
+        result += frame_score
+        @card << { frame: crd_frm += 1, type: "OPEN", frame_score: frame_score, total: result }
         rollndx += 2
       end
       
--- spec/game_test.rb
+++ spec/game_test.rb
@@ -8,6 +8,7 @@ class GameTest < Minitest::Test
 
   def test_game_class_exists
     assert_instance_of Game, @game
+    assert_equal [], @game.card
   end
 
   def test_gutter_game
@@ -38,6 +39,43 @@ class GameTest < Minitest::Test
   def test_perfect_game
     roll_many_of_same(12, Game::PINS)
     assert_equal 300, @game.score
+  end
+  def test_card_strike
+    strike
+    @game.score
+    assert @game.card.any? { |f| f[:frame] == 1 && f[:type] == "STRIKE" }
+  end
+
+  def test_complete_card
+    roll_many_of_same(20,0)
+    @game.score
+    assert @game.card.any? { |f| f[:frame] == 10 && f[:type] == "OPEN" }
+  end
+
+  
+
+  # Did the bowler get a spare or strike in the tenth?
+  def test_tenth_frame_is_strike
+    roll_many_of_same(18,0)
+    strike
+    @game.score
+    assert @game.tenth_frame_strike?
+  end
+
+  def test_tenth_frame_bonus_rolls_added
+    roll_many_of_same(10, Game::PINS)
+    roll_two(4,4)
+    assert_equal 282, @game.score
+  end
+
+  def print_random_card
+    Game::FRAMES.times do 
+      r1 = rand(0..9)
+      r1 <= 7 ? r2 = 3 : r2 = 1
+      roll_two(r1, r2)
+      print "|#{r1},#{r2}"
+    end
+    print "|TOTAL=#{@game.score}\n" 
   end
 
   private
