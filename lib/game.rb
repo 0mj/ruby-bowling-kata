a1cb046c532bfccee3e505a57e8cc22cb338ae11
Matt Jamison <thirdshiftvideo@gmail.com>
2025-06-03 11:59:23 -0400

    practice

--- lib/game.rb
+++ lib/game.rb
@@ -1,15 +1,18 @@
 class Game
   FRAMES = 10
   PINS = 10
+
   def initialize
     @rolls = []
   end
+
   def roll(pins)
     @rolls << pins
   end
+
   def score
     result = 0
-    roll_index = 0 
+    roll_index = 0
 
     FRAMES.times do
       if strike?(roll_index)
@@ -18,25 +21,26 @@ class Game
       elsif spare?(roll_index)
         result += spare_bonus(roll_index)
         roll_index += 2
-      else
-        result += @rolls.fetch(roll_index, 0) + @rolls.fetch(roll_index + 1,0) 
+      else 
+        result += @rolls.fetch(roll_index, 0) + @rolls.fetch(roll_index + 1, 0)
         roll_index += 2
       end
+      
     end
     result
   end
 
   private
-  def strike?(roll_index)
-    @rolls.fetch(roll_index, 0) == PINS #STRIKE!
-  end
-  def strike_bonus(roll_index)
-    PINS + @rolls.fetch(roll_index + 1, 0) + @rolls.fetch(roll_index + 2, 0)
-  end
   def spare?(roll_index)
-    @rolls.fetch(roll_index, 0) + @rolls.fetch(roll_index + 1,0) == PINS #spare!
+    @rolls.fetch(roll_index, 0) + @rolls.fetch(roll_index + 1, 0) == PINS #spare
   end
   def spare_bonus(roll_index)
-    PINS + @rolls.fetch(roll_index + 2, 0)
+    PINS + @rolls.fetch(roll_index + 2, 0) 
+  end
+  def strike?(roll_index)
+    @rolls.fetch(roll_index, 0) == PINS # strike
+  end
+  def strike_bonus(roll_index)
+    PINS + @rolls.fetch(roll_index + 1, 0) +@rolls.fetch(roll_index + 2, 0)
   end
 end
--- spec/game_test.rb
+++ spec/game_test.rb
@@ -1,56 +1,66 @@
 require 'minitest/autorun'
 require_relative '../lib/game'
 
-class GameTest < Minitest::Test
+
+class GameTest < Minitest::Test # MiniTest module (not a class) Test is the class within the MiniTest module
   def setup
     @game = Game.new
   end
 
   def test_game_class_exists
+    
     assert_instance_of Game, @game
   end
 
+  # 20 rolls of zero must score 0
   def test_gutter_game
+    
     roll_many(20,0)
     assert_equal 0, @game.score
   end
 
-  # test score bowler that rolls 4,3  5,3  6,1  and 14 gutters.  Should result in score of 22.
-  def test_open_frame_bowler
-    roll_frame(4,3)
-    roll_frame(5,3)
-    roll_frame(6,1)
-    roll_many(14,0)
-    assert_equal 22, @game.score
+  # ten rolls of one and ten rolls of zero must score 10
+  def test_ten_one_zero
+    roll_many(10,0)
+    roll_many(10,1)
+    assert_equal 10, @game.score
   end
 
+  # test spare 
+  # roll 9 1 9 & 17 gutters must score 28
   def test_spare
-    roll_frame(9,1) #spare
-    roll_frame(7,2)
-    roll_many(16,0)
-    assert_equal 26, @game.score
-  end
-
-  def test_strike
-    @game.roll(Game::PINS)
-    roll_frame(7,2)
-    roll_many(16,0)
+    roll_two(9,1) #spare
+    @game.roll(9) #bonus ball added to previous frame score
     assert_equal 28, @game.score
   end
 
-  def test_perfect_game
-    roll_many(12, Game::PINS)
-    assert_equal 300, @game.score
+  # two spares in a row
+  def test_spare_spare_open
+    roll_two(8,1)   # 9           = 9
+    roll_two(5,5)   # 9 + 10 + 4  = 23
+    roll_two(4,6)   # 23 + 10 + 1 = 34
+    roll_two(1,0)   # 34 + 1      = 35
+    roll_many(12,0)
+    assert_equal 35, @game.score
+  end
+
+  # test strike.  Roll 10, 3,4, and 16 gutters must score 24
+  def test_strike
+    @game.roll(Game::PINS)
+    roll_two(3,4)
+    assert_equal 24, @game.score
   end
 
   private
+  
   def roll_many(rolls, pins)
     rolls.times do
       @game.roll(pins)
     end
   end
-  def roll_frame(roll1, roll2)
-      @game.roll(roll1)
-      @game.roll(roll2)
+
+  def roll_two(r1, r2)
+    @game.roll(r1)
+    @game.roll(r2)
   end
 end
