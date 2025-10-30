ba1289cffd849a6192c554815576c72d12298485
Matt Jamison <mattjamisonxyz@gmail.com>
2025-10-30 16:35:52 -0400

    1030 practice

--- lib/game.rb
+++ lib/game.rb
@@ -1,63 +1,52 @@
 class Game
-  FRAMES = 10
-  PINS = 10
-  ROLLS_PER_NORMAL_FRAME = 2
-  STRIKE_ROLLS_USED = 1
-  NEXT_ROLL_OFFSET = 1
-  SECOND_NEXT_ROLL_OFFSET = 2
 
-  attr_accessor :rolls
+	PINS = 10
+	FRAMES = 10
 
-  def initialize
-    @rolls = []
-  end
+	def initialize
+		@rolls = []
+	end
 
-  def roll(pins)
-    raise ArgumentError, "Invalid number of pins" if pins < 0 || pins > 10
-    @rolls << pins
-  end
+	def roll(pins)
+		@rolls << pins
+	end
 
+	def score
+		current_r = 0 # current roll
+		result = 0
 
-  def score
-    result = 0
-    roll_index = 0
-    FRAMES.times do
-      frame_score, rolls_used = calculate_frame_score(roll_index)
-      result += frame_score
-      roll_index += rolls_used
-    end
-    result
-  end
+		FRAMES.times do
+			
+			if strike?(current_r)
+				result += strike_bonus(current_r)
+				current_r += 1
+			elsif spare?(current_r)
+				result += spare_bonus(current_r)
+				current_r += 2
+			else
+				result += open_frame(current_r)
+				current_r += 2
+			end
+			
+		end
+		result
+	end
 
+	private
 
- 
-
-
-  def calculate_frame_score(roll_index)
-    if strike?(roll_index)
-      [strike_bonus(roll_index), 1]
-    elsif spare?(roll_index)
-      [spare_bonus(roll_index), 2]
-    else
-      [normal_frame_score(roll_index), 2]
-    end
-  end
-
- 
-  def spare?(roll_index)
-    @rolls.fetch(roll_index, 0) + @rolls.fetch(roll_index + 1, 0) == PINS #spare
-  end
-  def spare_bonus(roll_index)
-    PINS + @rolls.fetch(roll_index + 2, 0) 
-  end
-  def strike?(roll_index)
-    @rolls.fetch(roll_index, 0) == PINS # strike
-  end
-  def strike_bonus(roll_index)
-    PINS + @rolls.fetch(roll_index + 1, 0) +@rolls.fetch(roll_index + 2, 0)
-  end
-  def normal_frame_score(roll_index)
-    @rolls.fetch(roll_index, 0) + @rolls.fetch(roll_index + 1, 0)
-
-  end
+	def spare?(current_r)
+		@rolls.fetch(current_r,0) + @rolls.fetch(current_r + 1,0) == PINS #spare
+	end
+	def spare_bonus(current_r)
+		PINS + @rolls.fetch(current_r + 2,0)
+	end
+	def open_frame(current_r)
+		@rolls.fetch(current_r,0) + @rolls.fetch(current_r + 1,0)
+	end
+	def strike?(current_r)
+		@rolls.fetch(current_r,0) == PINS #strike
+	end
+	def strike_bonus(current_r)
+		PINS +  @rolls.fetch(current_r + 1,0) + @rolls.fetch(current_r + 2,0)	
+	end
 end
